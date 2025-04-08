4bb0b6cdb7568c24df1871b6455ca7fbd4e4b4a3
andrewdaekim99 <ankim981693@gmail.com>
2025-04-08 16:50:45 -0700

    Add comments to models.py code for visibility and tracking.

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 
+# create table Note with 3 columns id(optional but primary key), title, and content
 class Note(SQLModel, table=True):
     id: int | None = Field(default=None, primary=True)
     title: str
     content: str
 
-sqlite_url = "sqlite:///./notes.db"
-engine = create_engine(sqlite_url, echo=True)
+sqlite_url = "sqlite:///./notes.db" # connects to a local SQLite DB file named notes.db
+engine = create_engine(sqlite_url, echo=True) # create the engine and echo = True will log all SQL commands to the terminal for debugging purposes
 
+# create database table if it doesn't already exist
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
