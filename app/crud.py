036fb61d5f0258af9e8a7dc48d172d08cea537bc
andrewdaekim99 <ankim981693@gmail.com>
2025-04-08 21:06:03 -0700

    updated all crud functions to require user id checks for security

--- app/crud.py
+++ app/crud.py
@@ -10,20 +10,23 @@ def create_note(note: Note):
         return note # return full saved object
     
 # returns all notes as a list by running SELECT * FROM note
-def get_notes():
+def get_notes_for_user(user_id: int):
     with Session(engine) as session:
-        return session.exec(select(Note)).all()
+        return session.exec(select(Note).where(Note.ownder_id == user_id)).all()
     
 # find and return note using primary key id
-def get_note(note_id: int):
+def get_note_by_id_for_user(note_id: int, user_id: int):
     with Session(engine) as session:
-        return session.get(Note, note_id)
+        note = session.get(Note, note_id)
+        if note and note.owner_id == user_id:
+            return note
+        return None
     
 # function to update an existing note in the db
-def update_note(note_id: int, data: dict):
+def update_note(note_id: int, data: dict, user_id: int):
     with Session(engine) as session:
         note = session.get(Note, note_id)  # retrieves specific note by id
-        if not note:    # if note id does not exist, return None
+        if not note or note.owner_id != user_id:    # if note id does not exist or does not belong to the user, return none
             return None
         for key, value in data.items(): # update object with new data and save
             setattr(note, key, value)
@@ -33,10 +36,10 @@ def update_note(note_id: int, data: dict):
         return note
 
 # delete an existing note in the database by id
-def delete_note(note_id: int):
+def delete_note(note_id: int, user_id: int):
     with Session(engine) as session:
         note = session.get(Note, note_id)
-        if not note:    # if note id doesn't exist, return None
+        if not note or note.owner_id != user_id:    # if note id doesn't exist or does not belong, return none
             return None
         session.delete(note) # delete note and commit change
         session.commit()
