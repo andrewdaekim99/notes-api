d3f3bd296b7b4ebf58b9bd8ed5362b26af046257
andrewdaekim99 <ankim981693@gmail.com>
2025-04-08 18:59:16 -0700

    Add a user class in the models file to create user objects with credentials so users can have ownership over their own notes.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from sqlmodel import Field, SQLModel, create_engine, Session, select, Relationship
+from typing import Optional, List
 
-# create table Note with 3 columns id(optional but primary key), title, and content
+# create a User class and table with credentials and ownership to maintain security and exclusivity for data
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str = Field(index=True, unique=True)
+    password_hash: str
+    notes: List["None"] = Relationship(back_populates="ownder")
+
+# create table Note with 5 columns id(optional but primary key), title, content, owner_id, and owner
 class Note(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str
     content: str
+    owner_id: int = Field(foreign_key="user.id")
+    owner: Optional[User] = Relationship(back_populates="notes")
 
 sqlite_url = "sqlite:///./notes.db" # connects to a local SQLite DB file named notes.db
 engine = create_engine(sqlite_url, echo=True) # create the engine and echo = True will log all SQL commands to the terminal for debugging purposes
