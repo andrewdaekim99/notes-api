e7b9b19f0bf4044e1efc53b19f1f3f90d65800e1
andrewdaekim99 <ankim981693@gmail.com>
2025-04-08 20:49:47 -0700

    Added 2 API endpoints to allow users to create an account with credentials and log in to receive a JWT token.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,44 @@
-from fastapi import FastAPI, HTTPException
-from app.models import Note, create_db_and_tables
+from fastapi import FastAPI, HTTPException, Depends
+from app.models import Note, create_db_and_tables, engine, User
 from app.crud import create_note, get_notes, get_note, update_note, delete_note
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from app.auth import hash_password, verify_password, create_access_token, decode_access_token
+from sqlmodel import select, Session
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()  # runs once when app starts to make sure tables exist
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# startup logic
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+     create_db_and_tables()
+     yield
+
+app = FastAPI(lifespan=lifespan)
+
+# accepts new unique usernames and passwords to create new users and add them to the database
+@app.post("/signup")
+def signup(form_data: OAuth2PasswordRequestForm = Depends()):
+     with Session(engine) as session:
+          existing_user = session.exec(select(User).where(User.username == form_data.username)).first()
+          if existing_user:
+               raise HTTPException(status_code=400, detail="Username already exists")
+          user = User(username=form_data.username, password_hash=hash_password(form_data.password))
+          session.add(user)
+          session.commit()
+          session.refresh(user)
+          return{"msg": "User created"}
+     
+# verifies user credentials and returns a JWT token is successful
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+     with Session(engine) as session:
+          user = session.exec(select(User).where(User.username == form_data.username)).first()
+          if not user or not verify_password(form_data.password, user.password_hash):
+               raise HTTPException(status_code=401, detail="Invalid credentials")
+          token = create_access_token({"sub": user.username})
+          return {"access_token": token, "token_type": "bearer"}
 
 # accepts a new note (validated by FastAPI), then creates and returns the saved note
 @app.post("/notes/", response_model=Note)
